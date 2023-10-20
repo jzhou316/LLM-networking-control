4e0690085af975622dc13c79ee5bd5ebfcb83161
Samuel Lin <samuellin@college.harvard.edu>
2023-10-20 10:05:12 -0400

    push changes to nile implementation

--- demo/home_network_client_demo.py
+++ demo/home_network_client_demo.py
@@ -126,7 +126,7 @@ def run():
 
 	#--------------------------------------- NETWORK CONFIG ---------------------------------------#
 
-	hosts = [{"name": "internet", "ip_address": "", "groups": [], "type": "internet"},
+	hosts = [{"name": "internet", "ip_address": "", "groups": ["internet"], "type": "internet"},
 			 {"name": "router", "ip_address": "192.168.1.1", "groups": ["network"], "type": "router"},	
 			 {"name": "motion-sensor", "ip_address": "192.168.1.2", "groups": ["network", "home-security-system"], "type": "device"},
 			 {"name": "alarm", "ip_address": "192.168.1.3", "groups": ["network", "home-security-system"], "type": "device"},
@@ -154,7 +154,7 @@ def run():
 			 {"link": ('router', 'printer'), "connection": "wired"}, 
 	]
 
-	groups = ['network', 'home-security-system', 'living-room']
+	groups = ['network', 'internet', 'home-security-system', 'living-room']
 	node_shapes = {"internet": "s", "router": "D", "firewall": "^", "device": "o"}
 
 
@@ -175,12 +175,20 @@ def run():
 			ai_msg = "add device('security-camera') to group('home-security-system')\nset policy('camera-traffic') { \n" + indent + "allow traffic(device('security-camera'), [device('phone'), group('home-security-system')])\n" + indent + "allow traffic([device('phone'), group('home-security-system')], device('security-camera'))\n}"
 		elif config_request == "I want to create a new subnet for my home office devices. This should include my work laptop, printer, and my phone. Also, make sure this subnet has priority access to the bandwidth during office hours.":
 			groups.append('home-office')
+			hosts_dict["work-laptop"]["groups"].append("home-office")
+			hosts_dict["printer"]["groups"].append("home-office")
+			hosts_dict["phone"]["groups"].append("home-office")
 			ai_msg = "add group('home-office')\nadd device('work-laptop') to group('home-office')\nadd device('printer') to group('home office')\nadd device('phone') to group('home office')\nset policy('office hours') {\n" + indent + "for group('home office') {\n" + (indent * 2)  + "from hour('09:00') to hour('17:00')\n" + (indent * 2) + "set bandwidth('min', '100', 'mbps')\n" + indent + "}\n}"
 		elif config_request == "I want to set up a guest Wi-Fi network that should only provide internet access and nothing more. It should also have limited bandwidth because I don't want it to get in the way of my main network's performance.":
-			ai_msg = "add group('guest network')\nset policy('guest bandwidth') {\n" + indent + "for group('guest network') {\n" + (indent * 2) + "set bandwidth('max', '5', 'mbps')\n" + indent + "}\n}"
+			groups.append('guest-network')
+			ai_msg = "add group('guest-network')\nset policy('guest bandwidth') {\n" + indent + "for group('guest-network') {\n" + (indent * 2) + "set bandwidth('max', '5', 'mbps')\n" + indent + "}\n}"
 		elif config_request == "My child does a lot of online gaming and it seems to be slowing down the internet for everyone else. Can you limit the amount of internet he can use?":
 			ai_msg = "set policy('gaming bandwidth') {\n" + indent + "for device('gaming console') {\n" + (indent * 2) + "set bandwidth('max', '5', 'mbps')\n" + indent + "}\n}"
 		elif config_request == "I've been hearing a lot about cyber threats on the news lately. I want to browse the web safely, but I don't want any strangers connecting to my devices from the internet.":
+			hosts_dict["firewall"] = {"name": "firewall",
+									  "ip-address": "192.168.1.12",
+									  "groups": ["network"],
+									  "type": "firewall"}
 			ai_msg = "add middlebox('firewall') to group('network')\n" + "set policy('web-browsing-security') {\n" + indent + "for middlebox('firewall') {\n" + (indent * 2) + "allow traffic(group('network'), group('internet'))\n" + (indent * 2) + "block traffic(group('internet'), group('network'))\n" + indent + "}\n}"
 		else:
 			print(config_request)
