83d7aef416f397e2f834b721128efcbd12faa0b4
Samuel <70851632+samuellin01@users.noreply.github.com>
2024-08-14 17:49:57 -0700

    Update assistant_handler.py

--- handlers/assistant_handler.py
+++ handlers/assistant_handler.py
@@ -1,8 +1,10 @@
 import time, os, asyncio
 from openai import OpenAI
+from dotenv import load_dotenv
 
 # Initialize OpenAI client
-OPENAI_API_KEY=""
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 # Paths to SONiC network configuration files
